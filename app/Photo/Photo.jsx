d10bf6ca29eb92c7d1879f10998304d5007d072a
Ryan Gomoto <ryan.gomoto@gmail.com>
2015-09-14 00:33:40 -0700

    Show filler image when real image is active

--- app/Photo/Photo.jsx
+++ app/Photo/Photo.jsx
@@ -7,14 +7,23 @@ var Photo = React.createClass({
       'photo': true,
       'active': this.props.isActive
     });
+    var fillerClasses = classNames({
+      'picture': true,
+      'filler': true,
+      'active': this.props.isActive
+    });
     return (
-      <div className={photoClasses}
-          onClick={this.props.onClick}>
-        <div className='photo-content'>
-          <img className='picture'
-               src={this.props.photo.picture}
-               alt={this.props.photo.text}/>
-          <p className='caption'>{this.props.photo.text}</p>
+      <div>
+        <img className={fillerClasses}
+             src={this.props.photo.picture}/>
+        <div className={photoClasses}
+             onClick={this.props.onClick}>
+          <div className='photo-content'>
+            <img className='picture'
+                 src={this.props.photo.picture}
+                 alt={this.props.photo.text}/>
+            <p className='caption'>{this.props.photo.text}</p>
+          </div>
         </div>
       </div>
     );
