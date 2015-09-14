3adc71e481883019f012094c6d4df389c0e8dd77
Ryan Gomoto <ryan.gomoto@gmail.com>
2015-09-13 22:19:22 -0700

    Set clicked photo as active

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,12 @@
 var React = require('react');
 var reqwest = require('reqwest');
+var classNames = require('classnames');
 
 var Gallery = React.createClass({
   getInitialState: function() {
     return {
-      photos: []
+      photos: [],
+      activePhoto: null
     };
   },
 
@@ -27,13 +29,34 @@ var Gallery = React.createClass({
     );
   },
 
+  handlePhotoClick: function(photoId) {
+    if (this.state.activePhoto === photoId) {
+      this.setState({
+        activePhoto: null
+      });
+    } else {
+      this.setState({
+        activePhoto: photoId
+      });
+    }
+  },
+
   render: function() {
     var photos = this.state.photos.map(function(photo) {
-      return <img className="photo" photoId={photo.id} src={photo.picture} alt={photo.text}/>;
-    });
+      var photoClasses = classNames({
+        'photo': true,
+        'active': photo.id === this.state.activePhoto
+      });
+      return <img className={photoClasses}
+                  photoId={photo.id}
+                  src={photo.picture}
+                  alt={photo.text}
+                  onClick={this.handlePhotoClick.bind(null, photo.id)}
+                  key={photo.id}/>;
+    }.bind(this));
 
     return (
-      <div className="gallery">
+      <div className='gallery'>
         {photos}
       </div>
     );
